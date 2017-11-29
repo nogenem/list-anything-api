ebea1a16e8a00ddcfc1cd116d8afd47953201e16
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-11-29 17:20:42 -0200

    Terminar a parte base do login

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import Promise from "bluebird";
+
+import auth from "./routes/auth";
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true });
+
+app.use("/api/auth", auth);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
